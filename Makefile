96f208ce5fc473cd81f8634460c53155324b8f03
pothub <aaaa1421421@gmail.com>
2016-02-25 21:53:59 +0900

    all part success

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ OBJ		= main.o
 PROGRAM	= out
 .PHONY: out
 $(PROGRAM) : $(OBJ)
-	gcc -o $(PROGRAM) $(OBJ) -lwiringPi
+	gcc -o $(PROGRAM) $(OBJ) -lwiringPi `pkg-config opencv --libs` -lm
+	sudo ./out
--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
+#include <wiringPiSPI.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+CvCapture *capture;
+char *wname = "camera";
+IplImage *img_origin,*imgResult;
+IplImage *img[3],*imgThreshold[3],*imgTmp;
+#define wi 320
+#define hi 240
 #define RANGE 100
+#define CS 24
 void set_motor(){
 	softPwmCreate(4,0,RANGE);
 	softPwmCreate(17,0,RANGE);
@@ -45,24 +57,44 @@ void led(int num, int onoff){
 		else digitalWrite(7,1);
 	}
 }
-void set_led(){
+void set_gpio(){
 	pinMode(23,OUTPUT);	//bz
+	pinMode(15,OUTPUT);	//air
 	pinMode(25,OUTPUT);
 	pinMode(8,OUTPUT);
 	pinMode(7,OUTPUT);
-	led(0,0);
-	led(1,0);
-	led(2,0);
+	digitalWrite(25,0);
+	digitalWrite(8,0);
+	digitalWrite(7,0);
 }
 void bz(int onoff){
 	if(onoff) digitalWrite(23,1);
 	else digitalWrite(23,0);
 }
+void air(int onoff){
+	if(onoff) digitalWrite(15,1);
+	else digitalWrite(15,0);
+}
+void set_spi(){
+	wiringPiSPISetup(0,100000);
+	pinMode(CS,OUTPUT);
+}
+int read_adc(unsigned char adcChannel) {
+	unsigned char buff[3];
+	int adcValue = 0;
+	buff[0] = 0x06 + (adcChannel >> 2);
+	buff[1] = adcChannel << 6;
+	buff[2] = 0x00;
+	digitalWrite(CS, 0); // Low : CS Active
+	wiringPiSPIDataRW(0, buff, 3);
+	buff[1] = 0x0F & buff[1];
+	adcValue = ( buff[1] << 8) | buff[2];
+	digitalWrite(CS, 1); // High : CS Inactive
+	return adcValue;
+}
 int fd;
 short int ax,ay,az,gx,gy,gz;
 void set_mpu(){
-	pinMode(8,OUTPUT);
-	pinMode(9,OUTPUT);
 	if((fd=wiringPiI2CSetup(0x68))<0){
 		printf("can't find i2c\n");
 		return;
@@ -77,15 +109,94 @@ void update_mpu(){
 	gy = wiringPiI2CReadReg8(fd,0x45)<<8|wiringPiI2CReadReg8(fd,0x46);
 	gz = wiringPiI2CReadReg8(fd,0x47)<<8|wiringPiI2CReadReg8(fd,0x48);
 }
+void set_servo(){
+	pinMode(18,PWM_OUTPUT);
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetClock(400);
+	pwmSetRange(1024);
+}
+void setcam(){
+	capture = cvCreateCameraCapture(-1);
+	if (capture==NULL) {
+		puts("*ERR* cvCreateCameraCapture");
+		return;
+	}
+	cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_WIDTH, wi);
+	cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_HEIGHT, hi);
+
+	int i;
+	for(i=0;i<3;i++){
+		img[i] 			= cvCreateImage(cvSize(wi,hi),IPL_DEPTH_8U,1);
+		imgThreshold[i] 	= cvCreateImage(cvSize(wi,hi),IPL_DEPTH_8U,1);
+	}
+	imgTmp = cvCreateImage(cvSize(wi,hi),IPL_DEPTH_8U,1);
+	imgResult = cvCreateImage(cvSize(wi,hi),IPL_DEPTH_8U,1);
+}
+int takePicture(const char *path){
+	// img_origin = cvQueryFrame(capture);
+	if( !img_origin ){
+		puts("*ERR* cvQueryFrame");
+		return -2;
+	}
+	if( !cvSaveImage(path, imgResult, NULL) ){
+		puts("*ERR* cvSaveImage");
+		return -3;
+	}
+	return 0;
+}
+void excolor(){
+	if((img_origin = cvQueryFrame(capture)) == NULL) return;
+	cvSplit(img_origin,img[2],img[1],img[0],NULL);//BGR
+
+	cvThreshold(img[2],imgThreshold[2],100,255,CV_THRESH_BINARY);
+	cvDiv(img[2],img[1],imgTmp,10);
+	cvThreshold(imgTmp,imgThreshold[1],10,255,CV_THRESH_BINARY);
+	cvDiv(img[2],img[0],imgTmp,10);
+	cvThreshold(imgTmp,imgThreshold[0],10,255,CV_THRESH_BINARY);
+	cvAnd(imgThreshold[1],imgThreshold[0],imgTmp,NULL);
+	cvAnd(imgTmp,imgThreshold[2],imgResult,NULL);
+}
 int main(){
-	if(wiringPiSetupGpio() == -1) return -1;
+	wiringPiSetupGpio();
 	set_motor();
-	set_led();
 	set_mpu();
+	set_spi();
+	set_servo();
+	set_gpio();
 	bz(1);
 	delay(100);
 	bz(0);
 
+	char path[256];
+	int i=0;
+	setcam();
+	for(;i<10;i++){
+		sprintf(path, "photo/cam%05d.jpg", i);
+		excolor();
+		takePicture(path);
+	}
+	cvReleaseCapture(&capture);
+	return 0;
+
+	while(1){
+		air(0);
+		// pwmWrite(18,30);
+		delay(1000);
+		air(1);
+		// pwmWrite(18,100);
+		delay(2000);
+	}
+
+	while(1){
+		int i[8]={0},num=0;
+		for(num=0;num<8;num++){
+			i[num]=read_adc(num);
+			printf("%d\t",i[num]);
+			delay(1);
+		}
+		printf("\n");
+		delay(100);
+	}
 	while(1){
 		update_mpu();
 		printf("%d\t%d\t%d\t%d\t%d\t%d\n",ax,ay,az,gx,gy,gz);
